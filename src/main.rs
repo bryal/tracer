a349b5ab2c75b2699f7e5a0adcbc68d3f79da7f3
JoJo <jo@jo.zone>
2019-11-12 22:44:10 +0100

    Change default window size

--- src/main.rs
+++ src/main.rs
@@ -10,38 +10,24 @@ use luminance_glfw::{
 const VERT_SHADER_SRC: &'static str = include_str!("vert.glsl");
 const FRAG_SHADER_SRC: &'static str = include_str!("frag.glsl");
 
-// Vertex semantics. Those are needed to instruct the GPU how to select vertex’s
-// attributes from the memory we fill at render time, in shaders. Acts as
-// "protocol" between GPU's memory regions and shaders.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Semantics)]
 pub enum Semantics {
     #[sem(name = "pos", repr = "[f32; 2]", wrapper = "VertexPosition")]
     Position,
-    #[sem(name = "color", repr = "[u8; 3]", wrapper = "VertexColor")]
-    Color,
 }
 
-// We derive the Vertex trait automatically and we associate to each field the
-// semantics that must be used on the GPU. The proc-macro derive Vertex will
-// make sure for us every field we use have a mapping to the type you specified
-// as semantics.
-//
-// Currently, we need to use #[repr(C))] to ensure Rust is not going to move
-// struct's fields around.
 #[repr(C)]
 #[derive(Clone, Copy, Debug, PartialEq, Vertex)]
 #[vertex(sem = "Semantics")]
 struct Vertex {
     pos: VertexPosition,
-    #[vertex(normalized = "true")]
-    rgb: VertexColor,
 }
 
 fn main() {
     // Surface to render to and get events from.
     let mut surface = GlfwSurface::new(
-        WindowDim::Windowed(960, 540),
-        "Hello, world!",
+        WindowDim::Windowed(800, 800),
+        "Tracer",
         WindowOpt::default(),
     )
     .expect("GLFW surface creation");
